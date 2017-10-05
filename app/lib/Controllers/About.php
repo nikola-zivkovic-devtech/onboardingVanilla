f88b90ba1372b462df41b5fe09a0d2b9a0117c9c
Nikola Zivkovic <nikola.zivkovic@devtechgroup.com>
2017-10-05 13:08:00 +0200

    Added some namespaced classes.

---
+++ app/lib/Controllers/About.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikola.zivkovic
+ * Date: 05-Oct-17
+ * Time: 12:42
+ */
+
+namespace Controllers;
+
+
+class About {
+
+    public function __construct() {
+        echo "About.";
+    }
+
+}
